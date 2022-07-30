875f21a803af26a6fd751926800875461a046e19
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-07-30 19:06:08 -0300

    Finalizado o IMC part 1

--- app/src/main/java/maxson/com/br/gordox/MainActivity.kt
+++ app/src/main/java/maxson/com/br/gordox/MainActivity.kt
@@ -3,14 +3,41 @@ package maxson.com.br.gordox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
         Log.w("Lifecycle", "Create - Estou criando a tela")
     }
 
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_LONG).show()
+        }
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            //titleTXT?.text = text.toString()
+        }
+        calcularBTN.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "IMC: $imc"
+        }
+    }
+
+/*
     override fun onStart() {
         super.onStart()
         Log.w("Lyfecycle","Start - Deixei a tela visível para você")
@@ -40,4 +67,6 @@ class MainActivity : AppCompatActivity() {
         Log.w("Lyfecycle","\nDestroy - Oh No!! The Screen foi destruída!!")
         super.onDestroy()
     }
+ */
+
 }
